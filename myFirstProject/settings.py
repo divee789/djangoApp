0949605617a3a1ccb736410e17a95024d3f625fa
Divine Olokor <divee789@gmail.com>
2020-07-03 05:31:13 +0100

    todo api for read and creater

--- myFirstProject/settings.py
+++ myFirstProject/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',          
+    'rest_framework', 
     'pages.apps.PagesConfig',
     'todo.apps.TodoConfig',
     'django.contrib.admin',
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+     'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
