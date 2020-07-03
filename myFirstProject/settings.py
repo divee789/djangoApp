ed28399fd34202c5fc9d5548b8833588ef05c3da
Divine Olokor <divee789@gmail.com>
2020-07-03 04:28:44 +0100

    setup for todo api

--- myFirstProject/settings.py
+++ myFirstProject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'todo.apps.TodoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
